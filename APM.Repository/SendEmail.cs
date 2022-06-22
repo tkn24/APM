5967c0e2514380904b77defccbae8794c8217c6c
ilker Tekin <tekin.ilker24@gmail.com>
2022-06-22 22:30:21 +0300

    EMail

--- APM.Repository/SendEmail.cs
+++ APM.Repository/SendEmail.cs
@@ -12,11 +12,11 @@ namespace APM.Repository
         public void Send(string Subject, string Email, string Body)
         {
             // kimlik bilgileri
-            var credentials = new NetworkCredential("erdo992@gmail.com", "qozduyaactczzqtr");
+            var credentials = new NetworkCredential("*****@gmail.com", "password");
 
             var mail = new MailMessage()
             {
-                From = new MailAddress("erdo992@gmail.com"),
+                From = new MailAddress("*****@gmail.com"),
                 Subject = Subject,
                 Body = Body
             };
